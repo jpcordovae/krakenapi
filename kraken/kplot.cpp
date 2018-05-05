fe30d5a367f25f8e0663784525b5737e8237aced
Juan Pablo Cordova E <jpcordovae@gmail.com>
2018-05-04 22:08:52 -0300

    adding kplot.* files for gnuplot integration.

---
+++ kraken/kplot.cpp
@@ -0,0 +1,7 @@
+#include "kplot.hpp"
+
+Gnuplot gp;
+
+void plot_init(){
+  
+}
---
+++ kraken/kplot.hpp
@@ -0,0 +1,10 @@
+#ifndef KPLOT_HPP_
+#define KPLOT_HPP_
+
+#include "gnuplot-iostream.h"
+
+extern Gnuplot gp;
+
+void plot_init();
+
+#endif
