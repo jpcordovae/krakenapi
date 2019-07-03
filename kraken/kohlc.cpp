3f20155a95b5bf64e71475176098f7668e4ce84d
Juan Pablo Cordova E <jpcordovae@gmail.com>
2019-07-03 14:55:45 +0000

    kohlc ostream fix

---
+++ kraken/kohlc.cpp
@@ -0,0 +1,23 @@
+#include "kohlc.hpp"
+
+namespace kraken {
+
+  std::ostream& operator<<(std::ostream &os, const KOHLC &kohlc) {
+    os << to_string(kohlc.ktime) << " ; "
+       << kohlc.open << " ; "
+       << kohlc.close << " ; "
+       << kohlc.high << " ; "
+       << kohlc.low << " ; "
+       << kohlc.vwap << " ; "
+       << kohlc.volume << " ; "
+       << kohlc.count;
+    return os;
+  }
+
+  std::ostream& operator<<(std::ostream &os, const KOHLCStorage &KS) {
+    std::for_each(KS.begin(),KS.end(),[&os](const KOHLC &kohlcs){
+	os << kohlcs << endl;
+      });
+    return os;
+  }
+};
