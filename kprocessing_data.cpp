721d8c3a1b4bb446c1d2d049ad5154f5572b74f9
Juan Pablo Cordova E <jpcordovae@gmail.com>
2019-10-01 19:57:04 +0300

    update

--- kprocessing_data.cpp
+++ kprocessing_data.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <memory>
 #include <regex>
+#include <algorithm>
 
 #include "kraken/kutils.hpp"
 #include "kraken/kclient.hpp"
@@ -143,22 +144,22 @@ int main(int argc, char **argv)
   std::cout << "total spreads : " << kss.size() << std::endl;
 
   /***************************************************/
-  Gnuplot gp(">script.gp");
-  // Create a script which can be manually fed into gnuplot later:
-  //    Gnuplot gp(">script.gp");
-  // Create script and also feed to gnuplot:
-  //    Gnuplot gp("tee plot.gp | gnuplot -persist");
-  // Or choose any of those options at runtime by setting the GNUPLOT_IOSTREAM_CMD
-  // environment variable.
-
+  //Gnuplot gp(">script.gp");
+  Gnuplot gp;
+  gp << "set grid nopolar\n";
+  gp << "set grid xtics nomxtics ytics nomytics noztics nomztics nortics nomrtics nox2tics nomx2tics noy2tics nomy2tics nocbtics nomcbtics\n";
+  gp << "set grid layerdefault   lt 0 linecolor 0 linewidth 0.500,  lt 0 linecolor 0 linewidth 0.500\n";
+  gp << "set style increment default\n";
+  gp << "set ytics  norangelimit logscale\n";
   // Gnuplot vectors (i.e. arrows) require four columns: (x,y,dx,dy)
-  std::vector<boost::tuple<double, double, double, double> > pts_A;
-  std::vector<boost::tuple<double, double, double, double> > points;
+  //std::vector<boost::tuple<double, double, double, double> > pts_A;
+  //std::vector<boost::tuple<double, double, double, double> > points;
+  std::vector<boost::tuple<double, double, double, double, double, double, double> > mdata;
   // You can also use a separate container for each column, like so:
-  std::vector<double> pts_B_x;
-  std::vector<double> pts_B_y;
-  std::vector<double> pts_B_dx;
-  std::vector<double> pts_B_dy;
+  //std::vector<double> pts_B_x;
+  //std::vector<double> pts_B_y;
+  //std::vector<double> pts_B_dx;
+  //std::vector<double> pts_B_dy;
 
   // You could also use:
   //   std::vector<std::vector<double> >
@@ -168,7 +169,7 @@ int main(int argc, char **argv)
   //   blitz::Array<blitz::TinyVector<double, 4>, 1> (with the Blitz++ library)
   // ... or anything of that sort
 
-  for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
+  /*for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
     double theta = alpha*2.0*3.14159;
     pts_A.push_back(boost::make_tuple(
                                       cos(theta),
@@ -181,14 +182,22 @@ int main(int argc, char **argv)
     pts_B_y .push_back( sin(theta)*0.8);
     pts_B_dx.push_back( sin(theta)*0.1);
     pts_B_dy.push_back(-cos(theta)*0.1);
-  }
-
+    }*/
+  std::vector<time_t> timestamp;
+  std::vector<double> volumes;
+  std::vector<double> prices;
+  std::for_each(kts.begin(),kts.end(),[&](KTrade &kt) {
+      timestamp.push_back(kt.time);
+      prices.push_back(kt.price);
+      volumes.push_back(kt.volume);
+    });
   // Don't forget to put "\n" at the end of each line!
-  gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
+  //gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
   // '-' means read from stdin.  The send1d() function sends data to gnuplot's stdin.
-  gp << "plot '-' with vectors title 'pts_A', '-' with vectors title 'pts_B'\n";
-  gp.send1d(pts_A);
-  gp.send1d(boost::make_tuple(pts_B_x, pts_B_y, pts_B_dx, pts_B_dy));
+  //gp << "plot '-' with vectors title 'pts_A', '-' with vectors title 'pts_B'\n";
+  gp << "plot '-' with vectors title 'pts_A'\n";
+  gp.send1d(std::make_tuple(timestamp,prices));
+  //gp.send1d(boost::make_tuple(pts_B_x, pts_B_y, pts_B_dx, pts_B_dy));
 
 #ifdef _WIN32
   // For Windows, prompt for a keystroke before the Gnuplot object goes out of scope so that
